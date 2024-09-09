ec29d2c713bd6dd1270c7342262b28283eb062de
Ben Kircher <bkircher@0xadd.de>
2024-09-09 20:21:21 +0200

    Add Markdown rendering to echo

--- echo.py
+++ echo.py
@@ -1,11 +1,16 @@
 import sys
 
+import requests
 
+from rich.console import Console
+from rich.markdown import Markdown
+
+console = Console()
 
 def main():
-    args = sys.argv[1:]
-    # var = input()
-    print(" ".join(args))
+    args = sys.argv[-1]
+    resp = requests.get(args)
+    console.print(Markdown(resp.text))
 
 
 if __name__ == "__main__":
